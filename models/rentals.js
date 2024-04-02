ecfdd6a69421733ec892b4859f30367ed99a7b31
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-04-02 15:02:33 +0200

    ObjectId validation using Joi

--- models/rentals.js
+++ models/rentals.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 // Schema and model
 const Rental = mongoose.model('Rental', new mongoose.Schema({
@@ -58,8 +59,8 @@ const Rental = mongoose.model('Rental', new mongoose.Schema({
   
 function validateRental(rental) {
     const schema = Joi.object({
-        customerId: Joi.string().required(),
-        movieId: Joi.string().required()
+        customerId: Joi.objectId().required(),
+        movieId: Joi.objectId().required()
       });
   
     return result = schema.validate(rental);
