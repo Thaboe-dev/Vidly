66a594717609e0fd4fc9d3a8f5e0cbe21b402f0c
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-04-04 05:46:47 +0200

    implemented the lodash library to minimize redundancy

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 
 // API Endpoints
 
@@ -13,16 +14,17 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    user = new User(_.pick(req.body, [
+        'name',
+        'email',
+        'password'
+    ]));
 
     // posting to the database
     try {
         await user.save();
-        res.send(user);
+        
+        res.send(_.pick(user, ['name', 'email']));
     }
     catch (ex) {
         res.send(ex.message);
