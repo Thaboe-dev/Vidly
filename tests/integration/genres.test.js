758ff9fdf1a6232a1e7d983ba525df644b6dee5f
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-04-17 15:25:36 +0200

    writing integration tests for genre routes

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const { Genre } = require('../../models/genres');
 let server;
 
 jest.mock('config', () => {
@@ -16,12 +17,38 @@ jest.mock('config', () => {
 
 describe('/api/genres', () => {
     beforeEach(() => {  server = require('../../app'); });
-    afterEach(() => { server.close(); });
+    afterEach(async () => { 
+        server.close();
+        await Genre.deleteMany({ name: 'genre1' });
+        await Genre.deleteMany({ name: 'genre2' });    
+    });
 
     describe('GET /', () => {
         it('should return all genres', async () => {
+            await Genre.collection.insertMany([
+                { name: 'genre1' },
+                { name: 'genre2' }
+            ]);
+
             const res = await request(server).get('/api/genres');
-            expect(res.status).toBe(200)
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+        
         });
+    });
+
+    describe('GET /:id', () => {
+        it('should return a genre if valid id is passed', async () => {
+            const genre = new Genre({ name: 'genre1' });
+            await genre.save();
+       
+            const res = await request(server).get('/api/genres/' + genre._id);
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', genre.name);
+        });
+
+        
     });
 });
