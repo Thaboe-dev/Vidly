cd9dc06baecd0b219d117c12c4bbdc818f19a79b
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-04-23 12:35:17 +0200

    add new route to server

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const movies = require('../routes/movies');
 const rentals = require('../routes/rentals');
 const users = require('../routes/user');
 const auth = require('../routes/auth');
+const returns = require('../routes/returns');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
@@ -16,6 +17,7 @@ module.exports = function(app) {
     app.use('/api/rentals', rentals);
     app.use('/api/users', users);
     app.use('/api/auth', auth);
+    app.use('/api/returns', returns);
 
     //express error middleware
     app.use(error);
