a49318ddab52f4007d98498ab9b0203c3fa2fbc4
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-04-24 18:44:17 +0200

    middleware for the movie routes

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { Movie, validate } = require('../models/movies');
 const { Genre } = require('../models/genres');
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
+const validateObjectId = require('../middleware/validateObjectId');
 const router = express.Router();
 
 // API Endpoints
@@ -48,7 +50,7 @@ router.post('/', auth, async (req, res) => {
 })
 
 // UPDATE
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', [auth, admin, validateObjectId], async (req, res) => {
     // Input Validation
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -73,7 +75,7 @@ router.put('/:id', auth, async (req, res) => {
 })
 
 // DELETE
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
     const movie = await Movie.findByIdAndDelete(req.params.id);
     // if invalid ID
     if (!movie) return res.status(404).send("Movie not found");
