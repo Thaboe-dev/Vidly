b4eebe32eb4e634f85d3bf1e6352d07052845506
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-04-24 18:48:19 +0200

    modified the users router

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const validateObjectId = require('../middleware/validateObjectId');
 
 // API Endpoints
 
@@ -39,10 +40,10 @@ router.post('/', async (req, res) => {
 });
 
 // Read
-router.get('/', async (req, res) => {
+`router.get('/', async (req, res) => {
     const users = await User.find().sort('name');
     res.send(users);
-});
+});`
 
 router.get('/me', auth, async (req, res) => {
 
@@ -51,7 +52,7 @@ router.get('/me', auth, async (req, res) => {
 });
 
 // Update
-router.put('/me', async (req, res) => {
+router.put('/me', [auth, validateObjectId], async (req, res) => {
     // Input Validation
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -67,7 +68,7 @@ router.put('/me', async (req, res) => {
 });
 
 // Delete
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, validateObjectId], async (req, res) => {
     const user = await User.findByIdAndDelete(req.params.id);
     if ((!user)) return res.status(404).send("User not found");
     res.send(user);
